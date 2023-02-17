89c71a2fac58200aaf9f2154b60151c5bef2e0fa
Chris Jobin <c.r.jobin@gmail.com>
2023-02-16 18:32:12 -0600

    Removed various unused files

---
+++ BlazorForms/BlazorForms/Server/Controllers/FormController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorForms.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FormController : ControllerBase
+    {
+
+    }
+}
